e6655734b6ef65708f3ab761e29f2d54e302353f
Andrew Hyder <hyd415@gmail.com>
2014-07-30 21:39:39 -0700

    Putting text messages on a web page.

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import datetime, os, requests, json
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
+@app.route("/", methods=["GET"])
+def index():
+    """ Show off the texts that have been sent today."""
+
+    messages = []
+    auth = (os.environ.get('TWILIO_ACCOUNT_SID'), os.environ.get('TWILIO_AUTH_TOKEN'))
+    r = requests.get("http://api.twilio.com/2010-04-01/Accounts/"+os.environ.get('TWILIO_ACCOUNT_SID')+"/Messages.json?DateSent>="+str(datetime.date.today()), auth=auth)
+    r = r.json()
+    for message in r['messages']:
+        messages.append(message['body'])
+    html = "<ul>"
+    for message in messages:
+        html += "<li>"+message+"<li>"
+    html += "</ul>"
+    return html
+
+@app.route("/incoming", methods=['GET', 'POST'])
+def incoming():
     """Respond to incoming calls with a simple text message."""
 
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message("Isn't that cute.")
     return str(resp)
 
 if __name__ == "__main__":
